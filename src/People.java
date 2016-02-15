776f2a28191393c3674ed8e3d3ff1d61ba123c3a
BrandenSandahl <branden157@gmail.com>
2016-02-15 18:48:48 -0500

    added a loop within a loop. then i took that out and added a Lambda expression.

--- src/People.java
+++ src/People.java
@@ -1,6 +1,4 @@
-
-import jodd.json.JsonSerializer;
-
+Adimport jodd.json.JsonSerializer;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -24,7 +22,17 @@ public class People {
             Collections.sort(personArray); //call custom sort method
         }
 
-        System.out.println(personMap);
+        System.out.println(personMap.values());
+
+        //just a way to play around with nested loops a bit more and getting inside of multi-D-data-structures
+        //Saw Alex messing around with .size and it made me think of it.
+        final int[] assetCount = {0};  //variables used in Lambda's must be final and arrays. Don't know why.
+        personMap.forEach(( k,v ) -> v.forEach(item-> assetCount[0] += 1));  //Lambda expression that runs to forEach's
+
+       // for (ArrayList<Person> personArray : personMap.values()) for (Person p : personArray) assetCount[0] += 1;
+
+        System.out.printf("There are a total of %d assets in %d countries", assetCount[0], personMap.keySet().size());
+
         try {
             writeToJson(personMap);
         } catch (IOException e) {
@@ -45,7 +53,7 @@ public class People {
         while (fileScanner.hasNext()) {
             //should be: [0]id [1]firstNam [2]lastName [3]email [4]country [5]ip
             String[] personSplit = fileScanner.nextLine().split(",");
-            String key = personSplit[4]; //this is where the country is in our file. Just makes it easier to work with
+            String key = personSplit[4]; //this is where the country is in our file. Just makes it easier to work with for me.
 
             //this is a bit long, but it's taking all of the fields in the array of a single ling from the file and creating a person object
             Person tempPerson = new Person(personSplit[1], personSplit[2], personSplit[3], personSplit[4], personSplit[5], Integer.parseInt(personSplit[0]));
