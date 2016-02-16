d0c0af693049d78e49ffb54009042f4832ecc034
BrandenSandahl <branden157@gmail.com>
2016-02-15 19:12:46 -0500

    Started making a Test file but i couldn't really figure out what I was testing exactly.

--- src/People.java
+++ src/People.java
@@ -3,10 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class People {
 
---
+++ src/Test/PeopleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by branden on 2/15/16 at 19:08.
+ */
+public class PeopleTest {
+
+    @Test
+    public void testFileToHashMap() {
+
+    }
+
+
+}
